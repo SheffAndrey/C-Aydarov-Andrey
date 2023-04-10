448bc8dbc66edf5fc326c9f75a971b1544bd4914
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-11 00:46:10 +0300

    Max digit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            Console.WriteLine("Введите второе числ");
+            string input2 = Console.ReadLine();
+            int number2 = int.Parse(input2);
+
+            if(number > number2){
+                Console.Write(number);
+                Console.WriteLine("Больше второго числа");
+            }
+            else{
+                Console.Write(number2);
+                Console.WriteLine("Больше первого числа");
+            }
+        }
+    }
+}
